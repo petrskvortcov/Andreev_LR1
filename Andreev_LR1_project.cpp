54de46dd97177ba02a9bbbe878dab7ce88ec68cf
petrskvortcov <petrskvortcov@yandex.ru>
2025-04-19 17:21:04 +0300

    limit

--- Andreev_LR1_project.cpp
+++ Andreev_LR1_project.cpp
@@ -1,32 +1,30 @@
 #include <iostream>
-#include <cmath>
+#include <cmath>   // Для pow()
+#include <limits>  // Для numeric_limits
 using namespace std;
 
-
-// Объявление функций с пустыми телами
+// Объявления функций
 void inputThreeDigitNumber(int &X);
 void inputDigitN(int &N);
 void findFirstDigit(int X);
 void findNDigit(int X, int N);
 
-int main()
-{
+int main() {
     int X = 0;
     int N = 0;
     int choice = 0;
 
     do {
-        cout << "Меню (почти как в ресторане):\n";
-        cout << "1. Ввести целое трёхзначное число X (стоит столько-то баллов)\n";
-        cout << "2. Ввести цифру N (меньше числа разрядов числа X (стоит столько-то баллов))\n";
-        cout << "3. Найти первую цифру числа X (стоит столько-то баллов)\n";
-        cout << "4. Найти N-ю цифру числа X (нумерация справа налево (стоит столько-то баллов))\n";
-        cout << "5. Выход (стоит 0 баллов)\n";
-        cout << "Татьяна Викторовна, выбор за Вами: ";
+        cout << "Меню:\n";
+        cout << "1. Ввести целое трёхзначное число X\n";
+        cout << "2. Ввести цифру N (меньше числа разрядов числа X)\n";
+        cout << "3. Найти первую цифру числа X\n";
+        cout << "4. Найти N-ю цифру числа X (нумерация справа налево)\n";
+        cout << "5. Выход\n";
+        cout << "Выбор: ";
         cin >> choice;
 
-        switch (choice)
-        {
+        switch (choice) {
             case 1:
                 inputThreeDigitNumber(X);
                 break;
@@ -43,52 +41,49 @@ int main()
                 cout << "Выход из программы.\n";
                 break;
             default:
-                cout << "Неверный выбор. Пожалуйста, попробуйте ещё раз.\n";
+                cout << "Неверный выбор. Повторите попытку.\n";
         }
-    } 
-    while (choice != 5);
+    } while (choice != 5);
 
     return 0;
 }
 
 // Функция для ввода трёхзначного числа
-void inputThreeDigitNumber(int &X)
-{
+void inputThreeDigitNumber(int &X) {
     cout << "Введите целое трёхзначное число: ";
-    cin >> X;
-    if (X < 100 || X > 999)
-    {
-        cout << "Ошибка: число должно быть только трёхзначным и никак иначе.\n";
-        X = 0; // Сбрасывание значения
+    while (!(cin >> X) || X < 100 || X > 999) {
+        cout << "Ошибка! Число должно быть трехзначным. Повторите попытку: ";
+        cin.clear();           // Очистка состояния потока
+        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Удаление неправильного ввода
     }
 }
 
 // Функция для ввода цифры N
 void inputDigitN(int &N) {
-    cout << "Введите цифру N (меньше числа разрядов числа X): ";
-    cin >> N;
-    if (N < 1 || N > 3) {
-        cout << "Ошибка: N должно быть в диапазоне от 1 до 3.\n";
-        N = 0; // Сбрасывание значения
+    cout << "Введите цифру N (от 1 до 3): ";
+    while (!(cin >> N) || N < 1 || N > 3) {
+        cout << "Ошибка! N должно быть от 1 до 3. Повторите попытку: ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
     }
 }
 
 // Функция для нахождения первой цифры числа X
 void findFirstDigit(int X) {
-    if (X != 0) {
-        int firstDigit = X / 100; // Первая цифра
+    if (X >= 100 && X <= 999) {
+        int firstDigit = X / 100;
         cout << "Первая цифра числа X: " << firstDigit << endl;
     } else {
-        cout << "Сначала введите трёхзначное число.\n";
+        cout << "Число X должно быть трёхзначным!\n";
     }
 }
 
 // Функция для нахождения N-й цифры числа X
 void findNDigit(int X, int N) {
-    if (X != 0 && N > 0) {
-        int nDigit = (X / static_cast<int>(pow(10, N - 1))) % 10; // N-я цифра
+    if (X >= 100 && X <= 999 && N > 0 && N <= 3) {
+        int nDigit = (X / static_cast<int>(pow(10, N - 1))) % 10;
         cout << N << "-я цифра числа X: " << nDigit << endl;
     } else {
-        cout << "Сначала введите трёхзначное число и цифру N.\n";
+        cout << "Некорректные данные: проверьте правильность чисел X и N\n";
     }
 }
